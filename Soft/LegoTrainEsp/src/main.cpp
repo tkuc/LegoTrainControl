106a2dc9ce3f49df5de3fd5eabc14218831ad82e
kucu <kuctomasz3@gmail.com>
2024-04-17 08:49:18 +0200

    throthle calibration

--- Soft/LegoTrainEsp/src/main.cpp
+++ Soft/LegoTrainEsp/src/main.cpp
@@ -4,6 +4,8 @@
 #include <Lpf2Hub.h>
 #include <Bounce2.h>
 #include "main.h"
+#include <EEPROM.h>
+#define EEPROM_SIZE 10
 
 // create a hub instance
 Lpf2Hub myTrainHub;
@@ -23,6 +25,17 @@ byte port = (byte)PoweredUpHubPort::A;
 
 #define LED_SET(ledPin, STATE) digitalWrite(ledPin, STATE)
 
+uint16_t calibAdcMin = 0;
+uint16_t calibAdcMax = 0;
+uint16_t calibAdcCenter = 0;
+
+uint8_t trainRolling = 0;
+uint8_t trainStopButton = 0;
+int16_t throthlePrev = 0;
+int16_t throthleStop = 0;
+uint8_t delayCntr = 0;
+uint8_t ledBlink = 0;
+
 Bounce buttonStop;
 Bounce buttonLight;
 Bounce buttonFuel;
@@ -32,16 +45,14 @@ int getThrothle(void);
 buttonAction_t checkButtons(void);
 int getPotADC(void);
 void calibCheck(void);
+void setColorButton(void);
+void playSoundButton(void);
+void rebfuelButton(void);
 
 void setup() {
   Serial.begin(115200);
+  EEPROM.begin(EEPROM_SIZE);
   //initialize train connection
-
-  //initialize buttons
-  // pinMode(BUTTON_STOP_PIN, INPUT_PULLUP);
-  // pinMode(BUTTON_LIGHT_PIN, INPUT_PULLUP);
-  // pinMode(BUTTON_FUEL_PIN, INPUT_PULLUP);
-  // pinMode(BUTTON_SOUND_PIN, INPUT_PULLUP);
 
   pinMode(LED_PWR_G, OUTPUT);
   pinMode(LED_CON_B, OUTPUT);
@@ -53,6 +64,7 @@ void setup() {
   delay(100);
   LED_SET(LED_CON_B, DISABLE_LED);
   LED_SET(LED_BAT_R, DISABLE_LED);
+
 
   //check if calibration mode enabled - 4 buttons pressed on startup
   calibCheck();
@@ -79,19 +91,35 @@ void setup() {
 
 // main loop
 void loop() {
+  int16_t throthle=0;
+  int16_t deltaThrothle = 0;
+
   switch(checkButtons())
   {
     case BUTTON_ACTION_STOP:
+      myTrainHub.playSound(3);
+      delay(100);
+      myTrainHub.playSound(3);
+      delay(100);
+      trainStopButton = 1;
+      trainRolling = 0;
+      throthleStop = throthlePrev;
+      myTrainHub.stopBasicMotor(port);
+      delay(100);
+      myTrainHub.stopBasicMotor(port);
       Serial.println("STOP pressed");
       break;
     case BUTTON_ACTION_LIGHT:
       Serial.println("LIGHT pressed");
+      setColorButton();
       break;
     case BUTTON_ACTION_FUEL:
       Serial.println("FUEL pressed");
+      rebfuelButton();
       break;
     case BUTTON_ACTION_SOUND: 
       Serial.println("SOUND pressed");
+      playSoundButton();
       break;
     case BUTTON_ACTION_NONE:
       /*no break*/
@@ -101,81 +129,198 @@ void loop() {
 
   getThrothle();
 
-//   //in case of loost connection - reinit
-//   if (!myTrainHub.isConnected() && !myTrainHub.isConnecting()) 
-//   {
-//     myTrainHub.init(2);
-//     //myTrainHub.init("90:84:2b:03:19:7f"); //example of initializing an hub with a specific address
-//   }
+  //in case of loost connection - reinit
+  if (!myTrainHub.isConnected() && !myTrainHub.isConnecting()) 
+  {
+    myTrainHub.init(2);
+    //myTrainHub.init("90:84:2b:03:19:7f"); //example of initializing an hub with a specific address
+  }
 
-//   // connect flow. Search for BLE services and try to connect if the uuid of the hub is found
-//   if (myTrainHub.isConnecting()) {
-//     myTrainHub.connectHub();
-//     if (myTrainHub.isConnected()) {
-//       Serial.println("Connected to HUB");
-//       Serial.print("Hub address: ");
-//       Serial.println(myTrainHub.getHubAddress().toString().c_str());
-//       Serial.print("Hub name: ");
-//       Serial.println(myTrainHub.getHubName().c_str());
-//     } else {
-//       Serial.println("Failed to connect to HUB");
-//     }
-//   }
+  // connect flow. Search for BLE services and try to connect if the uuid of the hub is found
+  if (myTrainHub.isConnecting()) {
+    myTrainHub.connectHub();
+    if (myTrainHub.isConnected()) {
+      Serial.println("Connected to HUB");
+      Serial.print("Hub address: ");
+      Serial.println(myTrainHub.getHubAddress().toString().c_str());
+      Serial.print("Hub name: ");
+      Serial.println(myTrainHub.getHubName().c_str());
+    } else {
+      Serial.println("Failed to connect to HUB");
+    }
+  }
 
-//   // if connected, you can set the name of the hub, the led color and shut it down
-//   if (myTrainHub.isConnected()) {
+  // if connected, you can set the name of the hub, the led color and shut it down
+  if (myTrainHub.isConnected()) {
+    if(++delayCntr > 10){
+      delayCntr = 0;
 
-//   //   char hubName[] = "myTrainHub";
-//   //   myTrainHub.setHubName(hubName);
+      throthle = getThrothle();
+      if(abs(throthleStop - throthle)>30){
+        trainStopButton = 0;  //restart train only when throthle moved more than 30%
+      }
+      throthlePrev = throthle;
+      if(!trainStopButton){
+        if(throthle > 5 || throthle < -5){ //speed greater than 5%
+          myTrainHub.setBasicMotorSpeed(port, throthle);
+          trainRolling = 1;
+        }else{
+          if(trainRolling){
+            myTrainHub.stopBasicMotor(port);
+            trainRolling = 0;
+          }
+        }
+      }
+    }
 
-//   myTrainHub.setBasicMotorSpeed(port, getThrothle());
+  //   char hubName[] = "myTrainHub";
+  //   myTrainHub.setHubName(hubName);
 
-//   // int adcValue = analogRead(ADC_THROTHLE_PIN);
-//   // Serial.print("ADC Value: ");
-//   // Serial.println(adcValue);
+  // int adcValue = analogRead(ADC_THROTHLE_PIN);
+  // Serial.print("ADC Value: ");
+  // Serial.println(adcValue);
 
-// // myTrainHub.setTachoMotorSpeed(port,50,100,BrakingStyle::FLOAT);
-// // delay(1000);
-// // myTrainHub.setTachoMotorSpeed(port,0,100,BrakingStyle::FLOAT);
-// // delay(1000);
+// myTrainHub.setTachoMotorSpeed(port,50,100,BrakingStyle::FLOAT);
+// delay(1000);
+// myTrainHub.setTachoMotorSpeed(port,0,100,BrakingStyle::FLOAT);
+// delay(1000);
 
-//   // myTrainHub.setBasicMotorSpeed(port, 35);
-//   // delay(1000);
-//   // myTrainHub.setBasicMotorSpeed(port, 100);
-//   // delay(1000);
-//   // myTrainHub.setBasicMotorSpeed(port, 50);
-//   // delay(1000);
-//   // myTrainHub.setBasicMotorSpeed(port, -50);
-//   // delay(1000);C:\!MEGA\Projekty\LegoTrain
-//   // myTrainHub.setBasicMotorSpeed(port, 0);
-//   // delay(1000);
-//     // myTrainHub.setLedColor(GREEN);
-//     // delay(1000);
-//     // myTrainHub.setLedColor(RED);
+  // myTrainHub.setBasicMotorSpeed(port, 35);
+  // delay(1000);
+  // myTrainHub.setBasicMotorSpeed(port, 100);
+  // delay(1000);
+  // myTrainHub.setBasicMotorSpeed(port, 50);
+  // delay(1000);
+  // myTrainHub.setBasicMotorSpeed(port, -50);
+  // delay(1000);C:\!MEGA\Projekty\LegoTrain
+  // myTrainHub.setBasicMotorSpeed(port, 0);
+  // delay(1000);
+    // myTrainHub.setLedColor(GREEN);
+    // delay(1000);
+    // myTrainHub.setLedColor(RED);
 
-//     // myTrainHub.setBasicMotorSpeed(port, -35);
-//     // delay(1000);
-//     // myTrainHub.stopBasicMotor(port);
-//     // delay(1000);
+    // myTrainHub.setBasicMotorSpeed(port, -35);
+    // delay(1000);
+    // myTrainHub.stopBasicMotor(port);
+    // delay(1000);
 
-//   } else {
-//     Serial.println("Train hub is disconnected");
-//     delay(100);
-//   }
+    if(ledBlink < 7){
+      LED_SET(LED_CON_B, ENABLE_LED);
+    } else {
+      LED_SET(LED_CON_B, DISABLE_LED);
+    }
+  } else {
+    Serial.println("Train hub is disconnected");
+    delay(100);
+  }
 
-  delay(200);
+  if(++ledBlink > 100){
+    ledBlink = 0;
+    LED_SET(LED_CON_B, DISABLE_LED);
+  }
+  delay(10);
   
 } // End of loop
 
 void calibCheck(void) {
   //all 4 buttons have to be pressed on startup to calibrate
+  //initialize buttons
+  uint16_t adcCenter;
+  uint16_t adcMin = 0xFFFF;
+  uint16_t adcMax = 0;
+  pinMode(BUTTON_STOP_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_LIGHT_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_FUEL_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_SOUND_PIN, INPUT_PULLUP);
+  delay(50);
+  if( !digitalRead(BUTTON_STOP_PIN) && 
+      !digitalRead(BUTTON_LIGHT_PIN) &&
+      !digitalRead(BUTTON_FUEL_PIN) &&
+      !digitalRead(BUTTON_SOUND_PIN)){
+        Serial.println("Calibration mode enabled");
+          LED_SET(LED_CON_B, ENABLE_LED);
+          LED_SET(LED_BAT_R, ENABLE_LED);
+          LED_SET(LED_PWR_G, ENABLE_LED);
+          while(!digitalRead(BUTTON_STOP_PIN)); //wait for button release
+          delay(200);
+          LED_SET(LED_CON_B, DISABLE_LED);
+          LED_SET(LED_BAT_R, DISABLE_LED);
+          LED_SET(LED_PWR_G, DISABLE_LED);
 
+          
+          while(digitalRead(BUTTON_STOP_PIN)){
+            adcCenter = analogRead(ADC_THROTHLE_PIN);
+            delay(10);
+          }
+          LED_SET(LED_CON_B, ENABLE_LED);
+          LED_SET(LED_BAT_R, ENABLE_LED);
+          LED_SET(LED_PWR_G, ENABLE_LED);
+          delay(500);
+          LED_SET(LED_BAT_R, DISABLE_LED);
+          LED_SET(LED_PWR_G, DISABLE_LED);
+          uint16_t adcRead;
+          while(digitalRead(BUTTON_STOP_PIN)){
+            adcRead = analogRead(ADC_THROTHLE_PIN);
+            if(adcRead > adcMax){
+              adcMax = adcRead;
+            }
+            if(adcRead < adcMin){
+              adcMin = adcRead;
+            }
+            delay(10);
+          }
+          LED_SET(LED_CON_B, ENABLE_LED);
+          LED_SET(LED_BAT_R, ENABLE_LED);
+          LED_SET(LED_PWR_G, ENABLE_LED);
+          delay(500);
+          LED_SET(LED_CON_B, DISABLE_LED);
+          LED_SET(LED_BAT_R, DISABLE_LED);
+          LED_SET(LED_PWR_G, DISABLE_LED);
+
+          Serial.println("New throthle ADC calibration value");
+          Serial.print("Adc Min:");
+          Serial.println(adcMin);
+          Serial.print("Adc Max:");
+          Serial.println(adcMax);
+          Serial.print("Adc Center:");
+          Serial.println(adcCenter);
+          //store data to eeprom
+          EEPROM.write(0, adcMin>>8);
+          EEPROM.write(1, adcMin&0xFF);
+          EEPROM.write(2, adcMax>>8);
+          EEPROM.write(3, adcMax&0xFF);
+          EEPROM.write(4, adcCenter>>8);
+          EEPROM.write(5, adcCenter&0xFF);
+          EEPROM.commit();
+          Serial.println("Write to EEPROM - done");
+      }
+  //read adc calibration
+  Serial.println("Read throthle ADC calibration data");
+  calibAdcMin = EEPROM.read(0)<<8;
+  calibAdcMin |= EEPROM.read(1);
+  calibAdcMax = EEPROM.read(2)<<8;
+  calibAdcMax |= EEPROM.read(3);
+  calibAdcCenter = EEPROM.read(4)<<8;
+  calibAdcCenter |= EEPROM.read(5);
+
+  Serial.print("Adc Min:");
+  Serial.println(calibAdcMin);
+  Serial.print("Adc Max:");
+  Serial.println(calibAdcMax);
+  Serial.print("Adc Center:");
+  Serial.println(calibAdcCenter);
+
+  LED_SET(LED_PWR_G, ENABLE_LED);
 }
 
 int getPotADC(void) {
   int adcValue = analogRead(ADC_THROTHLE_PIN);
   // Serial.println(adcValue);
-  return map(adcValue, 1500, 3000, -100, +100);
+  if(adcValue>calibAdcCenter){
+    return map(adcValue, calibAdcCenter, calibAdcMax, -5, +100); 
+  }else{
+    return map(adcValue, calibAdcMin, calibAdcCenter, -100, +5);
+  }
 }
 
 int getThrothle(void) {
@@ -188,7 +333,7 @@ int getThrothle(void) {
   }else{
     throthleOut = inScaledVal;
   }
-  Serial.println(throthleOut);
+  // Serial.println(throthleOut);
   return throthleOut;
 }
 
@@ -213,3 +358,36 @@ buttonAction_t checkButtons(void) {
   }
   return BUTTON_ACTION_NONE;
 }
+
+void setColorButton(void){
+  static Color actualColor = BLACK;
+  actualColor = static_cast<Color>((static_cast<int>(actualColor) + 1) % NUM_COLORS);
+  if(actualColor > WHITE){
+    actualColor = BLACK;
+  }
+  Serial.print("Set color");
+  Serial.println(actualColor);
+
+  myTrainHub.setLedColor(actualColor);
+}
+
+void playSoundButton(void){
+  static uint8_t sound = 0;
+  Serial.print("play sound");
+  myTrainHub.playSound(9);
+}
+
+void rebfuelButton(void){
+  static uint8_t sound = 0;
+  Serial.print("Refuel - play sound:7");
+  myTrainHub.playSound(7);
+  delay(1500);
+  myTrainHub.playSound(7);
+  delay(1500);
+  myTrainHub.playSound(7);
+  delay(1500);
+}
+//tankowanie sound 7
+//sound 9 huhu
+//sound 10 mijanie
+//sound 3 hamowanie
